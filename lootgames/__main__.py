89c9cfd4bb8089e90819a579c8550c6d8ef71aa6
loot! <axeliandrea666@gmail.com>
2025-09-21 00:19:51 +0700

    Update __main__.py

--- lootgames/__main__.py
+++ lootgames/__main__.py
@@ -7,7 +7,7 @@ from lootgames.modules import database_group as dbgroup
 # ================= CONFIG ================= #
 API_ID = 29580121      # isi API_ID
 API_HASH = "fff375a88f6546f0da2df781ca7725df"  # isi API_HASH
-BOT_TOKEN = "7660904765:AAFQuSU8ShpXAzqYqAhBojjGLf7U03ityck"" # isi BOT_TOKEN
+BOT_TOKEN = "7660904765:AAFQuSU8ShpXAzqYqAhBojjGLf7U03ityck" # isi BOT_TOKEN
 OWNER_ID = 6395738130
 ALLOWED_GROUP_ID = -1002904817520
 LOG_LEVEL = logging.INFO
@@ -54,3 +54,4 @@ if __name__ == "__main__":
         pass
 
     asyncio.run(main())
+
