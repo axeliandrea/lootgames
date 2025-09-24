0216849fc9527bce76600c22e61d27db3f9a3b73
loot! <axeliandrea666@gmail.com>
2025-09-24 07:51:14 +0700

    Update menu_utama.py

--- lootgames/modules/menu_utama.py
+++ lootgames/modules/menu_utama.py
@@ -1,4 +1,4 @@
-# lootgames/modules/menu_utama.py
+# lootgames/modules/menu_utama.py FIXXXXXXXXXXX
 import logging
 import asyncio
 import re
@@ -787,3 +787,4 @@ def register(app: Client):
     app.add_handler(MessageHandler(handle_transfer_message, filters.text & filters.private))
     app.add_handler(CallbackQueryHandler(callback_handler))
     logger.info("[MENU] Handler menu_utama terdaftar.")
+
