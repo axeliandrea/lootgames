8b22b516d92b1caa3b37a7b1c1f6ee5016391a07
loot! <axeliandrea666@gmail.com>
2025-10-06 03:01:51 +0700

    Update menu_utama.py

--- lootgames/modules/menu_utama.py
+++ lootgames/modules/menu_utama.py
@@ -17,7 +17,7 @@ from datetime import date
 
 logger = logging.getLogger(__name__)
 OWNER_ID = 6395738130
-TARGET_GROUP = -1002946278772  # ganti sesuai supergroup bot
+TARGET_GROUP = -1002904817520  # ganti sesuai supergroup bot
 
 # ---------------- STATE ---------------- #
 TRANSFER_STATE = {}       # user_id: {"jenis": "A/B/C/D"}
