d423f8555650eafa68238594c9f86c35da02fbb4
loot! <axeliandrea666@gmail.com>
2025-10-08 06:02:26 +0700

    Update __main__.py

--- lootgames/__main__.py
+++ lootgames/__main__.py
@@ -1,4 +1,4 @@
-# lootgames/__main__.py revisi full terbaru
+# lootgames/__main__.py revisi full terbaru 1
 import asyncio
 import logging
 import os
@@ -127,3 +127,4 @@ if __name__ == "__main__":
         pass
 
     asyncio.run(main())
+
