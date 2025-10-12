06f43eea6979adacf90bf8b13bd551f1e31be084
loot! <axeliandrea666@gmail.com>
2025-10-12 07:50:02 +0700

    Update menu_utama.py

--- lootgames/modules/menu_utama.py
+++ lootgames/modules/menu_utama.py
@@ -1,4 +1,4 @@
-#FIX 07:16 KALO MAU GANTI INI
+#FIX 05:52
 # lootgames/modules/menu_utama.py Test Nonaktif Umpan Rare
 import os
 import logging
@@ -2189,7 +2189,6 @@ def register(app: Client):
     # --- Logging tambahan ---
     logger.info("💬 menu_utama handlers registered (callback + tc_drop_input)")
     print("[DEBUG] register(menu_utama) dipanggil ✅")
-
 
 
 
