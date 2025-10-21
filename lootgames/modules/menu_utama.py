6ee659120c052063977ae0fb28cd0889e73f8c4d
loot! <axeliandrea666@gmail.com>
2025-10-21 23:10:22 +0700

    Update menu_utama.py

--- lootgames/modules/menu_utama.py
+++ lootgames/modules/menu_utama.py
@@ -947,7 +947,7 @@ MENU_STRUCTURE = {
             ("⬅️ Back", "D")
         ]
     },
-    "D3LA": {
+    "D3A": {
         "title": "📋 🔄 POINT CHAT",
         "buttons": [
             ("TUKAR 🔄 UMPAN COMMON 🐛", "TUKAR_POINT"),
@@ -2606,5 +2606,6 @@ def register_sedekah_handlers(app: Client):
     app.add_handler(MessageHandler(handle_sedekah_input, filters.private & filters.text))
     app.add_handler(CallbackQueryHandler(callback_handler))
     print("[DEBUG] register_sedekah_handlers() aktif ✅")
+
 
 
