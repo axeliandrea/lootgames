427b0a613f4c18bf70a11596b6ab8ae95d20bc5e
loot! <axeliandrea666@gmail.com>
2025-10-27 21:45:50 +0700

    Update menu_utama.py

--- lootgames/modules/menu_utama.py
+++ lootgames/modules/menu_utama.py
@@ -1,4 +1,4 @@
-#   # 
+# t  # 
 # lootgames/modules/menu_utama.py
 import os
 import time  # pastikan ada di top imports
@@ -2792,6 +2792,7 @@ def register_sedekah_handlers(app: Client):
     app.add_handler(MessageHandler(handle_sedekah_input, filters.private & filters.text))
     app.add_handler(CallbackQueryHandler(callback_handler))
     print("[DEBUG] register_sedekah_handlers() aktif ✅")
+
 
 
 
